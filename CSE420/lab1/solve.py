92b76efeac7b035a1cf5e1fd6964c890afe04e08
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-08-10 01:01:19 +0600

    :recycle: Refactor code

--- CSE420/lab1/solve.py
+++ CSE420/lab1/solve.py
@@ -8,8 +8,10 @@ OTHERS_REGEX = ",|\;|\|\(|\)|\{|\}|\[|\]"
 IDENTIFIER_REGEX = "^[A-Za-z_][A-Za-z0-9_]*"
 NUMERIC_REGEX = "[-]?([\d]*[\.]?[\d]+)"
 
+
 class Solve:
-    def __init__(self, file_path):
+
+    def __init__(self, file_path:str):
         self.file = file_path
         self.keyword_list = set()
         self.math_operator_list = set()
@@ -18,20 +20,17 @@ class Solve:
         self.identifier_list = set()
         self.numeric_list = set()
 
-    def splitter(self, string:str)->List:
+    def analyze(self):
+        with open(self.file, 'r') as file:
+            Lines = file.readlines()
+            for line in Lines:
+                self.parser(line)
+
+    def splitter(self, string:str) -> List:
         string.strip()
         return string.split()
 
-    def remove_symbol(self, string):
-        symbols = "+-*/++--,;><>=<==="
-        if (len(string) > 1):
-            if string[-1] in symbols:
-                string = string[:-1]
-            elif string[0] in symbols:
-                string = string[1:]
-        return string
-
-    def matcher(self,string,regex,items:set, identifier_of_numeric=False)->List:
+    def matcher(self, string:str, regex:str, items:set, identifier_of_numeric=False )->List:
         if identifier_of_numeric:
             if re.match(KEYWORD_REGEX, string) or re.match(OTHERS_REGEX, string):
                 return items
@@ -42,8 +41,8 @@ class Solve:
         if matched_string:
             items.add(matched_string.group())
         return items
-    
-    def parser(self, line):
+
+    def parser(self, line:str):
         for word in self.splitter(line):
             self.matcher(word, KEYWORD_REGEX, self.keyword_list)
             self.matcher(word, MATH_OPERATOR_REGEX, self.math_operator_list)
@@ -52,7 +51,16 @@ class Solve:
             self.matcher(word, IDENTIFIER_REGEX, self.identifier_list, identifier_of_numeric=True)
             self.matcher(word, NUMERIC_REGEX, self.numeric_list, identifier_of_numeric=True)
 
-    def printer(self, name, lst):
+    def remove_symbol(self, string:str):
+        symbols = "+-*/++--,;><>=<==="
+        if (len(string) > 1):
+            if string[-1] in symbols:
+                string = string[:-1]
+            elif string[0] in symbols:
+                string = string[1:]
+        return string
+
+    def printer(self, name:str, lst:set):
         print(name, end =": ");
         list_to_set = list(lst);
         if(len(list_to_set) > 0): print(list_to_set[0], end = " ")
@@ -67,12 +75,6 @@ class Solve:
         self.printer("Logical Operators", self.logical_operator_list)        
         self.printer("Numerical Values", self.numeric_list)   
         self.printer("Others", self.others_list)   
-
-    def analyze(self):
-        with open(self.file, 'r') as file:
-            Lines = file.readlines()
-            for line in Lines:
-                self.parser(line)
 
 
 if __name__ == "__main__":
